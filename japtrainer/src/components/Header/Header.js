e514305adce45afb4379483af8abe5f8867c8663
Roger Tan <teochewthunder@gmail.com>
2021-09-24 22:15:02 +0800

    Add files via upload

--- japtrainer/src/components/Header/Header.js
+++ japtrainer/src/components/Header/Header.js
@@ -8,18 +8,30 @@ function Header(props) {
 	let setCharset = props.setCharset;
 	let started = props.started;
 	let remaining = props.remaining;
+	let maxRemaining = props.maxRemaining;
+	let usedQuestions = props.usedQuestions;
 	let setStarted = props.setStarted;
 	let setRemaining = props.setRemaining;
+	let setAnswer = props.setAnswer;
 	let setResult = props.setResult;
+	let setUsedQuestions = props.setUsedQuestions;
+	let setQuestion = props.setQuestion;
 
 	const BtnStart_click = ()=> {
 		setStarted(true);
-		setRemaining(5);
+		setRemaining(maxRemaining);
 		setResult(0);
+		setUsedQuestions([]);
+		setQuestion(null);
 	};
 
 	const BtnEnd_click = ()=> {
 		setStarted(false);
+		setCharset(null);
+		setAnswer("");
+		setResult(0);
+		setUsedQuestions([]);
+		setQuestion(null);
 	};
 
 	const SetCurrentCharset = (name)=> {
@@ -30,12 +42,13 @@ function Header(props) {
 		return (
 			<>
 				<div className="Title">
-
+					<h1>J-Trainer</h1>
 				</div>
 				<div className="Controls">
 					<div>
+						<br />
 						<button onClick={ ()=>{BtnEnd_click();}}>
-							{ remaining === 0 ? "もう一度繰 ➤ " : "やめましょう ☓" }
+							{ remaining === 0 ? "RESTART ➤ " : "CANCEL ☓" }
 						</button>
 					</div>
 				</div>
@@ -45,14 +58,15 @@ function Header(props) {
 		return (
 			<>
 				<div className="Title">
-
+					<h1>J-Trainer</h1>
 				</div>
 				<div className="Controls">
 					<div>
-				        <input type="radio" name="charset" onClick={()=>{SetCurrentCharset('hiragana');}} selected={ charset === GetCharset('hiragana') } /> Hiragana <br />
-				        <input type="radio" name="charset" onClick={()=>{SetCurrentCharset('katakana');}} selected={ charset === GetCharset('katakana') } /> Katakana <br />
+				        <input type="radio" name="charset" onClick={()=>{SetCurrentCharset('hiragana');}}  /> Hiragana <br />
+				        <input type="radio" name="charset" onClick={()=>{SetCurrentCharset('katakana');}} /> Katakana <br />
+				        <br />
 						<button onClick={ ()=>{BtnStart_click();}} disabled={ charset === null ? true : false }>
-							始めましょう ➤ 
+							BEGIN ➤ 
 						</button>
 					</div>
 				</div>
