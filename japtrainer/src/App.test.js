40af6c40d97eb62fc73c3820803eae2d61a37021
Roger Tan <teochewthunder@gmail.com>
2022-01-07 21:25:20 +0800

    Add files via upload

--- japtrainer/src/App.test.js
+++ japtrainer/src/App.test.js
@@ -1,4 +1,10 @@
+import React from "react";
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-
+describe("App", () => {
+	it('renders', () => {
+		render(<App />);
+		expect(screen.queryByText('Welcome to J-Trainer!')).toBeInTheDocument();
+	});
+});
